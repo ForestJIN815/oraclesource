0194d2684d9761b871bc43080bd8182bd7261a8a
진효찬 <attattahugen@gmail.com>
2024-09-11 15:25:37 +0900

    메세지 적절하게

--- system.sql
+++ system.sql
@@ -23,3 +23,24 @@ DEFAULT TABLESPACE USERS
 TEMPORARY TABLESPACE TEMP
 QUOTA 10M ON USERS;
 -- ~~~~~ 스키마 생성되고 'C##TEST1' 라는 이름으로 만들어졌다
+
+-- 연결 권한 부여
+GRANT CREATE SESSION TO C##TEST1;
+
+-- 비밀번호 변경
+ALTER USER C##TEST1 IDENTIFIED BY 54321;
+
+-- 사용자 삭제
+-- CASCADE : 사용자와 객체를 모두 삭제
+-- DROP USER c##test1 CASCADE; -- 바꿔줘야 함
+
+-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ 복사
+-- 권한 권리
+-- 1) 시스템 권한
+--    사용자 생성, 수정, 삭제, 데이터베이스 접근, 오라클 데이터베이스 객체 생성 관리 권한
+-- 	  ex) 데이터베이스 접속 권한 : GRANT CREATE SESSION TO C##TEST1;
+-- 2) 객체 권한
+--~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ 복사
+
+-- c##test1 : 접속, 테이블 권한 부여
+CREATE USER SESSION, CREATE TABLE TO C##TEST1;
